e238b5a6afe47efd6730eae04de2595ecd506668
ajp109 <57534418+ajp109@users.noreply.github.com>
2022-08-11 12:02:51 +0100

    Correct code in main.cpp

--- main.cpp
+++ main.cpp
@@ -1,7 +1,20 @@
 #include "mbed.h"
 
+DigitalOut green(D2);
+DigitalOut red(D3);
+
+Thread redThread;
+
+void redThreadFn() {
+    
+    // Flash the red LED here
+    
+}
+
 int main() {
-    while (true) {
-        sleep();
-    }
+    
+    redThread.start(redThreadFn);
+    
+    // Flash the green LED here
+    
 }
